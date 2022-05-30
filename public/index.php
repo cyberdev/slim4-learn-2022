ca91cd23848109cf95abe02df85f573ddd0b8716
Nasrul Fatoni <nasrul.fatoni@gmail.com>
2022-05-30 11:58:40 +0700

    before and after middle ware

--- public/index.php
+++ public/index.php
@@ -1,41 +1,44 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Factory\AppFactory;
+use Slim\Psr7\Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
 /**
- * The routing middleware should be added earlier than the ErrorMiddleware
- * Otherwise exceptions thrown from it will not be handled by the middleware
- */
-$app->addRoutingMiddleware();
-
-/**
- * Add Error Middleware
+ * Example middleware closure
  *
- * @param bool                  $displayErrorDetails -> Should be set to false in production
- * @param bool                  $logErrors -> Parameter is passed to the default ErrorHandler
- * @param bool                  $logErrorDetails -> Display error details in error log
- * @param LoggerInterface|null  $logger -> Optional PSR-3 Logger
+ * @param  ServerRequest  $request PSR-7 request
+ * @param  RequestHandler $handler PSR-15 request handler
  *
- * Note: This middleware should be added last. It will not handle any exceptions/errors
- * for middleware added after it.
+ * @return Response
  */
-$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$beforeMiddleware = function (Request $request, RequestHandler $handler) {
+	$response = $handler->handle($request);
+	$existingContent = (string) $response->getBody();
 
+	$response = new Response();
+	$response->getBody()->write('BEFORE ' . $existingContent);
+
+	return $response;
+};
+
+$afterMiddleware = function ($request, $handler) {
+	$response = $handler->handle($request);
+	$response->getBody()->write(' AFTER');
+	return $response;
+};
+
+$app->add($beforeMiddleware);
+$app->add($afterMiddleware);
+
+// ...
 $app->get('/', function (Request $request, Response $response, $args) {
 	$response->getBody()->write("Hello world!");
 	return $response;
-});
-
-$app->get('/json', function (Request $request, Response $response, array $args) {
-	$payload = json_encode(['hello' => 'world'], JSON_PRETTY_PRINT);
-	$response->getBody()->write($payload);
-
-	return $response->withHeader('Content-Type', 'application/json')->withStatus(401);
 });
 
 $app->run();
